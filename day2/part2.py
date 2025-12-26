f1b233d0e0998751e3f5f1e6a578e9a702263c4d
A200315862 <Pranav.Hingane@t-systems.com>
2025-12-26 23:10:25 +0530

    added updated part2 file

--- day2/part2.py
+++ day2/part2.py
@@ -1,20 +1,20 @@
 def part2():
     total=0
-    with open('/Users/a200315862/Downloads/aoc/data/day2.txt','r') as file:
+    with open('/Users/A200315862/Downloads/aoc/data/day2.txt','r') as file:
         ranges = [list(map(int, i.split('-'))) for i in file.readline().strip().split(',')]
         for i in ranges:
             for j in range(i[0], i[1] + 1):
                 mid = len(str(j)) // 2
-                mango = len(str(j)) // 3
-                peru = len(str(j)) // 5
-                pen = len(str(j)) // 7
+                thrice = len(str(j)) // 3
+                fifth = len(str(j)) // 5
+                seventh = len(str(j)) // 7
                 if str(j)[:mid] == str(j)[mid:]:
                     total += j 
-                elif str(j)[:mango] == str(j)[mango : mango + mango] == str(j)[mango + mango : ]:
+                elif str(j)[:thrice] == str(j)[thrice : thrice + thrice] == str(j)[thrice + thrice : ]:
                     total += j
-                elif str(j)[:peru] == str(j)[peru : peru + peru] == str(j)[peru + peru : peru + peru + peru] == str(j)[peru + peru + peru : peru + peru + peru + peru] == str(j)[peru + peru + peru + peru :]:
+                elif str(j)[:fifth] == str(j)[fifth : fifth + fifth] == str(j)[fifth + fifth : fifth + fifth + fifth] == str(j)[fifth + fifth + fifth : fifth + fifth + fifth + fifth] == str(j)[fifth + fifth + fifth + fifth :]:
                     total += j
-                elif str(j)[:pen] == str(j)[pen : pen + pen] == str(j)[pen + pen : pen + pen + pen] == str(j)[pen + pen + pen : pen + pen + pen + pen] == str(j)[pen + pen + pen + pen : pen + pen + pen + pen + pen] == str(j)[pen + pen + pen + pen + pen : pen + pen + pen + pen + pen + pen] == str(j)[pen + pen + pen + pen + pen + pen:]:
+                elif str(j)[:seventh] == str(j)[seventh : seventh + seventh] == str(j)[seventh + seventh : seventh + seventh + seventh] == str(j)[seventh + seventh + seventh : seventh + seventh + seventh + seventh] == str(j)[seventh + seventh + seventh + seventh : seventh + seventh + seventh + seventh + seventh] == str(j)[seventh + seventh + seventh + seventh + seventh : seventh + seventh + seventh + seventh + seventh + seventh] == str(j)[seventh + seventh + seventh + seventh + seventh + seventh:]:
                     total += j
         print(total)
 if __name__=="__main__":
