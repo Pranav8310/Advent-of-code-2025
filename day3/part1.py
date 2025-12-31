cdebe847afcc5c839c47b834b75a299a21072855
A200315862 <Pranav.Hingane@t-systems.com>
2025-12-31 15:19:47 +0530

    added day3/part1.py file

---
+++ day3/part1.py
@@ -0,0 +1,14 @@
+def part1():
+    total = 0
+    with open('/Users/A200315862/Downloads/aoc/data/day3.txt','r') as fi:
+        for file in fi:
+            file = list(map(int, file.strip()))
+            tens = max(file[:-1])
+            first = max(file[file.index(tens) + 1:])
+            total += tens * 10 + first
+        print(total)
+
+
+if __name__ == '__main__':
+    part1() 
+
