c9e575bd9a9c118bdb56e359cdffda47ff55ff5e
A200315862 <Pranav.Hingane@t-systems.com>
2026-01-06 23:07:22 +0530

    added day4/part1.py file

---
+++ day4/part1.py
@@ -0,0 +1,13 @@
+def foo():
+    count = 0
+    files = [list(line.strip()) for line in open('/Users/A200315862/Downloads/aoc/aoc2025/data/day4.txt', 'r')]
+    for c, col in enumerate(files):
+        for r, row in enumerate(col):
+            if row == '@':
+                region = [subarray[max(0, r - 1): r + 2] for subarray in files[max(0, c - 1): c + 2]]
+                if sum(col.count("@") for col in region) <= 4:
+                    count += 1
+    print(count)                    
+
+if __name__ == '__main__':
+    foo()
