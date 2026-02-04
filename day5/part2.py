f9ac26db84144eba164bddd5d84a7e431f9bb3d4
A200315862 <Pranav.Hingane@t-systems.com>
2026-02-04 18:44:37 +0530

    changes in day5/part2.py file

--- day5/part2.py
+++ day5/part2.py
@@ -1,5 +1,5 @@
 def bar():
-    ranges= open('/Users/A200315862/Downloads/aoc/aoc2025/data/day5.txt').read()
+    ranges= open('aoc/aoc2025/data/day5.txt').read()
     ranges = [tuple(map(int, r.split("-"))) for r in ranges.splitlines()]
     ranges.sort()
     last = None
