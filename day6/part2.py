ca2400d7add5bc847b1f646cbf8629e623ed4528
A200315862 <Pranav.Hingane@t-systems.com>
2026-02-19 13:16:00 +0530

    added day6/part2.py file

---
+++ day6/part2.py
@@ -0,0 +1,20 @@
+def foo():
+    a = [file.strip("\n") for file in open("/Users/A200315862/Downloads/aoc/aoc2025/data/day6.txt",'r')]
+    cols = list(zip(*a))
+    groups = []
+    group = []
+    for col in cols:
+        if set(col) == {" "}:
+            groups.append(group)
+            group = []
+        else:
+            group.append(col)
+    groups.append(group)
+    total = 0
+    for group in groups:
+        total += eval(group[0][-1].join("".join(line[:-1]) for line in group))
+    print(total)
+
+
+if __name__=="__main__":
+    foo()
